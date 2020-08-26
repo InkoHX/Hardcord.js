9216ff1c94e8209b2069fb4bdf52c821c8c926b2
InkoHX <inkohx@gmail.com>
2020-08-26 23:54:34 +0900

    メソッド名を変更

--- src/CommandBuilder.ts
+++ src/CommandBuilder.ts
@@ -25,27 +25,27 @@ export interface Builder<F = {}> {
   default<K extends keyof F, V = F[K]>(key: K, value: V): Builder<Omit<F, K> & { [key in K]: V }>
   default<K extends string, V>(key: K, value: V): Builder<F & { [key in K]: V }>
 
-  toNumber<K extends keyof F>(key: K): Builder<Omit<F, K> & { [key in K]?: number }>
-  toNumber<K extends string>(key: K): Builder<F & { [key in K]?: number }>
+  number<K extends keyof F>(key: K): Builder<Omit<F, K> & { [key in K]?: number }>
+  number<K extends string>(key: K): Builder<F & { [key in K]?: number }>
 
-  toBoolean<K extends keyof F>(key: K): Builder<Omit<F, K> & { [key in K]?: boolean }>
-  toBoolean<K extends string>(key: K): Builder<F & { [key in K]?: boolean }>
+  boolean<K extends keyof F>(key: K): Builder<Omit<F, K> & { [key in K]?: boolean }>
+  boolean<K extends string>(key: K): Builder<F & { [key in K]?: boolean }>
 
-  toString<K extends keyof F>(key: K): Builder<Omit<F, K> & { [key in K]?: string }>
-  toString<K extends string>(key: K): Builder<F & { [key in K]?: string }>
+  string<K extends keyof F>(key: K): Builder<Omit<F, K> & { [key in K]?: string }>
+  string<K extends string>(key: K): Builder<F & { [key in K]?: string }>
 
-  toArray<K extends keyof F>(key: K): Builder<Omit<F, K> & { [key in K]: ReadonlyArray<NonNullable<F[key]>> }>
-  toArray<K extends string>(key: K): Builder<F & { [key in K]?: ReadonlyArray<string | number> }>
-  toArray<K extends keyof F>(keys: ReadonlyArray<K>): Builder<Omit<F, K> & { [key in K]: ReadonlyArray<NonNullable<F[key]>> }>
-  toArray<K extends string>(keys: ReadonlyArray<K>): Builder<F & { [key in K]?: ReadonlyArray<string | number> }>
+  array<K extends keyof F>(key: K): Builder<Omit<F, K> & { [key in K]: ReadonlyArray<NonNullable<F[key]>> }>
+  array<K extends string>(key: K): Builder<F & { [key in K]?: ReadonlyArray<string | number> }>
+  array<K extends keyof F>(keys: ReadonlyArray<K>): Builder<Omit<F, K> & { [key in K]: ReadonlyArray<NonNullable<F[key]>> }>
+  array<K extends string>(keys: ReadonlyArray<K>): Builder<F & { [key in K]?: ReadonlyArray<string | number> }>
 
-  toCount<K extends keyof F>(key: K): Builder<Omit<F, K> & { [key in K]: number }>
-  toCount<K extends string>(key: K): Builder<F & { [key in K]: number }>
-  toCount<K extends keyof F>(keys: ReadonlyArray<K>): Builder<Omit<F, K> & { [key in K]: number }>
-  toCount<K extends string>(keys: ReadonlyArray<K>): Builder<F & { [key in K]: number }>
+  count<K extends keyof F>(key: K): Builder<Omit<F, K> & { [key in K]: number }>
+  count<K extends string>(key: K): Builder<F & { [key in K]: number }>
+  count<K extends keyof F>(keys: ReadonlyArray<K>): Builder<Omit<F, K> & { [key in K]: number }>
+  count<K extends string>(keys: ReadonlyArray<K>): Builder<F & { [key in K]: number }>
 
-  setAlias<KL extends keyof F, KS extends string>(longKey: KL, shortKey: KS): Builder<F & { [key in KS]: F[KL] }>
-  setAlias<KL extends keyof F, KS extends string>(longKey: KL, shortKeys: KS[]): Builder<F & { [key in keyof KS]: F[KL] }>
+  alias<KL extends keyof F, KS extends string>(longKey: KL, shortKey: KS): Builder<F & { [key in KS]: F[KL] }>
+  alias<KL extends keyof F, KS extends string>(longKey: KL, shortKeys: KS[]): Builder<F & { [key in keyof KS]: F[KL] }>
 
   setCommandHandler(fnc: CommandHandler<F>): Builder<F>
 
@@ -70,36 +70,36 @@ export class CommandBuilder implements Builder {
     return this
   }
 
-  public toNumber<K extends never>(key: K): Builder<Pick<{}, never> & { [key in K]?: number | undefined }>
-  public toNumber<K extends string>(key: K): Builder<{ [key in K]?: number | undefined }>
-  public toNumber(key: string) {
+  public number<K extends never>(key: K): Builder<Pick<{}, never> & { [key in K]?: number | undefined }>
+  public number<K extends string>(key: K): Builder<{ [key in K]?: number | undefined }>
+  public number(key: string) {
     this._numbers.push(key)
 
     return this
   }
 
-  public toBoolean<K extends never>(key: K): Builder<Pick<{}, never> & { [key in K]?: boolean | undefined }>
-  public toBoolean<K extends string>(key: K): Builder<{ [key in K]?: boolean | undefined }>
-  public toBoolean(key: string) {
+  public boolean<K extends never>(key: K): Builder<Pick<{}, never> & { [key in K]?: boolean | undefined }>
+  public boolean<K extends string>(key: K): Builder<{ [key in K]?: boolean | undefined }>
+  public boolean(key: string) {
     this._booleans.push(key)
 
     return this
   }
 
-  public toString<K extends never>(key: K): Builder<Pick<{}, never> & { [key in K]?: string | undefined }>
-  public toString<K extends string>(key: K): Builder<{ [key in K]?: string | undefined }>
-  public toString(key: string) {
+  public string<K extends never>(key: K): Builder<Pick<{}, never> & { [key in K]?: string | undefined }>
+  public string<K extends string>(key: K): Builder<{ [key in K]?: string | undefined }>
+  public string(key: string) {
     this._strings.push(key)
 
     return this
   }
 
   // @ts-expect-error
-  public toArray<K extends never>(key: K): Builder<Pick<{}, never> & { [key in K]: readonly NonNullable<{}[key]>[] }>
-  public toArray<K extends string>(key: K): Builder<{ [key in K]?: readonly (string | number)[] | undefined }>
-  public toArray<K extends never>(keys: readonly K[]): Builder<Pick<{}, never> & { [key in K]: readonly NonNullable<{}[key]>[] }>
-  public toArray<K extends string>(keys: readonly K[]): Builder<{ [key in K]?: readonly (string | number)[] | undefined }>
-  public toArray(keys: string | ReadonlyArray<string>) {
+  public array<K extends never>(key: K): Builder<Pick<{}, never> & { [key in K]: readonly NonNullable<{}[key]>[] }>
+  public array<K extends string>(key: K): Builder<{ [key in K]?: readonly (string | number)[] | undefined }>
+  public array<K extends never>(keys: readonly K[]): Builder<Pick<{}, never> & { [key in K]: readonly NonNullable<{}[key]>[] }>
+  public array<K extends string>(keys: readonly K[]): Builder<{ [key in K]?: readonly (string | number)[] | undefined }>
+  public array(keys: string | ReadonlyArray<string>) {
     if (typeof keys === 'string') this._arrays.push(keys)
     else keys.forEach(key => this._arrays.push(key))
 
@@ -107,20 +107,20 @@ export class CommandBuilder implements Builder {
   }
 
   // @ts-expect-error
-  toCount<K extends never>(key: K): Builder<Pick<{}, never> & { [key in K]: number }>
-  toCount<K extends string>(key: K): Builder<{ [key in K]: number }>
-  toCount<K extends never>(keys: readonly K[]): Builder<Pick<{}, never> & { [key in K]: number }>
-  toCount<K extends string>(keys: readonly K[]): Builder<{ [key in K]: number }>
-  toCount(keys: string | ReadonlyArray<string>) {
+  count<K extends never>(key: K): Builder<Pick<{}, never> & { [key in K]: number }>
+  count<K extends string>(key: K): Builder<{ [key in K]: number }>
+  count<K extends never>(keys: readonly K[]): Builder<Pick<{}, never> & { [key in K]: number }>
+  count<K extends string>(keys: readonly K[]): Builder<{ [key in K]: number }>
+  count(keys: string | ReadonlyArray<string>) {
     if (typeof keys === 'string') this._counts.push(keys)
     else keys.forEach(key => this._counts.push(key))
 
     return this
   }
 
-  public setAlias<KL extends never, KS extends string>(longKey: KL, shortKey: KS): Builder<{ [key in KS]: {}[KL] }>
-  public setAlias<KL extends never, KS extends string>(longKey: KL, shortKeys: KS[]): Builder<{ [key in keyof KS]: {}[KL] }>
-  public setAlias(longKey: string, shortKeys: string | string[]) {
+  public alias<KL extends never, KS extends string>(longKey: KL, shortKey: KS): Builder<{ [key in KS]: {}[KL] }>
+  public alias<KL extends never, KS extends string>(longKey: KL, shortKeys: KS[]): Builder<{ [key in keyof KS]: {}[KL] }>
+  public alias(longKey: string, shortKeys: string | string[]) {
     if (typeof shortKeys === 'string') {
       this._aliases[longKey] = [shortKeys]
     } else {
